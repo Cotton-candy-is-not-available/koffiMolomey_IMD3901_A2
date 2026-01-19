18a378116b5cd341096c0ab9b1b6eb16be88709f
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-01-19 00:13:39 -0500

    added custom models untextured

--- koffiMolomey_IMD3901_A2_project/Assets/Scripts/PlantsScripts/SeedSpawner.cs
+++ koffiMolomey_IMD3901_A2_project/Assets/Scripts/PlantsScripts/SeedSpawner.cs
@@ -8,7 +8,7 @@ public class SeedSpawner : MonoBehaviour
     public GameObject cabbageSeedPrefab;
 
     float yPos = 1.0f;
-
+    float zPos = -2.47199988f;
 
 
     // Update is called once per frame
@@ -17,11 +17,10 @@ public class SeedSpawner : MonoBehaviour
      //need to get correct range 
         for (int i = 0; i< numOfSeeds; i++)
         {
-            Vector3 carrotSeedSpawnPos = new Vector3(4.6f, yPos+=0.5f, -1.8f);//spawn within this range
+            Vector3 carrotSeedSpawnPos = new Vector3(4.8f, yPos, -1.2f);//spawn within this range
             Instantiate(carrotSeedPrefab, carrotSeedSpawnPos, Quaternion.identity);// spawn carrot seed
 
-
-            Vector3 cabbageSeedSpawnPos = new Vector3(4.6f, yPos+=0.5f, -2.8f);//spawn within this range
+            Vector3 cabbageSeedSpawnPos = new Vector3(4.76000023f, yPos, zPos+0.3f);//spawn within this range
             Instantiate(cabbageSeedPrefab, cabbageSeedSpawnPos, Quaternion.identity);// spawn cabbage seed
 
 

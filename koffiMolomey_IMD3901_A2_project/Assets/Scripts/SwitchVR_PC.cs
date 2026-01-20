2734dfd486079d215fc7ac7a7ede267dffa9aa71
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-01-20 16:01:57 -0500

    added grass texture

--- koffiMolomey_IMD3901_A2_project/Assets/Scripts/SwitchVR_PC.cs
+++ koffiMolomey_IMD3901_A2_project/Assets/Scripts/SwitchVR_PC.cs
@@ -6,7 +6,17 @@ public class SwitchVR_PC : MonoBehaviour
    public  GameObject VRObjects;
    public  GameObject PCObjects;
 
-    [SerializeField] bool toggle = false;
+   public Transform VRPlayerPos;
+   public Transform PCPlayerPos;
+
+
+    [SerializeField] bool toggle;
+
+    private void Start()
+    {
+        toggle = false;
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -22,12 +32,18 @@ public class SwitchVR_PC : MonoBehaviour
         {
             VRObjects.SetActive(true);
             PCObjects.SetActive(false);
+
+            //set VR PLayer to same position as PC Player
+            VRPlayerPos = PCPlayerPos;
         }
 
         else if (toggle == false)//Turn on PC mode
         {
             VRObjects.SetActive(false);
             PCObjects.SetActive(true);
+
+            //set VR PLayer to same position as PC Player
+            PCPlayerPos = VRPlayerPos;
         }
     }
 }

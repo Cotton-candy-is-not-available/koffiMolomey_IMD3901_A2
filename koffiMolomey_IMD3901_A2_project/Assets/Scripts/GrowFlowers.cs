9b70547262bed7088e3911f97e2decf360c68e89
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-01-26 11:34:16 -0500

    added working flower growing mechanic

--- koffiMolomey_IMD3901_A2_project/Assets/Scripts/GrowFlowers.cs
+++ koffiMolomey_IMD3901_A2_project/Assets/Scripts/GrowFlowers.cs
@@ -6,23 +6,21 @@ public class GrowFlowers : MonoBehaviour
 
     [SerializeField] float increaseHeightValue = 0.1f;
 
-    [SerializeField] float maxHeight = 5.0f;
+    [SerializeField] float maxHeight = 1.4f;
 
     private void OnParticleCollision(GameObject other)
     {
-        //if (Stem.transform.localScale.y <= maxHeight) { 
-        //    Stem.transform.localScale += new Vector3(0.0f, increaseHeightValue, 0.0f);
-        //    Debug.Log("Growwwwwww");
+        if (Stem.transform.localScale.y <= maxHeight)
+        {
+            Stem.transform.localScale += new Vector3(increaseHeightValue, increaseHeightValue, increaseHeightValue);
+            Debug.Log("Growwwwwww");
+            //keep going until reaches max height
 
-        //}
-        //else
-        //{
-        //    Debug.Log("Max reached");
-        //}
-
-        //Stem.transform.localScale += new Vector3(0.0f, 0.1f, 0.0f);//increase stem high by 0.1f
-
-        //kep going until reaches max height
+        }
+        else
+        {
+            Debug.Log("Max reached");
+        }
     }
 
 
